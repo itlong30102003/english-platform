46e308aa810130b29032fba6b2127c34e98e10fc
Tran Duc Long <tranduclong.se@gmail.com>
2025-08-21 22:48:59 +0700

    ..........

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,10 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: 'http://localhost:3000',
+        origin: [
+            'http://localhost:3000',
+            'https://englishplatform.onrender.com'
+        ],
         credentials: true,
     });
     await app.listen(3001);
